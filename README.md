ec3e9dbf55860eaaa9aef30d74d9169d7731fcde
MaTachi <dannie92@gmail.com>
2013-09-01 12:33:22 +0200

    Possible to make a database backup of a WP blog

--- README.md
+++ README.md
@@ -4,12 +4,31 @@ Download a website's files over FTP and the SQL database's tables and rows.
 
 License: [GNU GPL version 3 or later](LICENSE)
 
-## Download the files
+## Make a backup of a site's files
 
-1. Open [backuper.py](backuper.py) and add ftp address, username and password.
+The site's complete file structure will with the instructions below be
+downloaded to the directory backup.
+
+1. Open [config.ini](config.ini) and add your FTP details.
 2. `mkdir backup && cd backup`
 3. `./../backuper.py`
 
-## Download the database
+## Make a backup of a WordPress installation, including both files and
+database, on a shared host
 
-Not yet implemented.
+['WP Complete Backup'](http://wordpress.org/plugins/wp-complete-backup/) will
+make a database backup and save it in its plugin folder. Then will the complete
+file structure be downloaded over FTP, including WP Complete Backup's backup of
+the database. Not the prettiest solution, but it works fine if you are using a
+shared host.
+
+1. Install ['WP Complete
+   Backup'](http://wordpress.org/plugins/wp-complete-backup/) on your WordPress
+blog.
+2. Open the settings for WP Complete Backup, add your IP address and copy the
+   API key.
+3. Open [config.ini](config.ini), add your FTP details, configure the WP
+   Complete Backup settings and set `backup.wp_complete_backup` to `yes`.
+4. `mkdir backup && cd backup`
+5. `./../backuper.py`
+
--- backuper.py
+++ backuper.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
+import configparser
 import os
+import urllib.request
 from ftplib import FTP
 
 class AryaStarkFtp(FTP):
     """Extend ftplib.FTP with some extra functionality."""
-
-    def __init__(self):
-        super().__init__('FTP.EXAMPLE.COM', 'USERNAME', 'PASSWORD')
 
     def get_file_list(self):
         """Return a list of all files in the current folder."""
@@ -42,12 +41,59 @@ class AryaStarkFtp(FTP):
                 os.chdir('..')
             else:
                 self.save_file(f['name'])
+                pass
 
     def save_file(self, filename):
         """Save the given file from FTP's cwd to Python's cwd."""
         with open(filename, 'wb') as f:
             self.retrbinary('RETR ' + filename, f.write)
 
-ftp = AryaStarkFtp()
-ftp.save_files_in_folder()
-ftp.quit()
+    def delete_folder(self, foldername=None):
+        """Delete all files in the specified folder or in the cwd on the
+        FTP."""
+        files = None
+
+        if foldername:
+            files = [{'name': foldername, 'dict': True}]
+        else:
+            files = self.get_file_list()
+
+        for f in files:
+            if f['dict']:
+                self.cwd(f['name'])
+                self.delete_folder()
+                self.cwd('..')
+                self.rmd(f['name'])
+            else:
+                self.delete(f['name'])
+                pass
+
+    def delete_wp_complete_backups(self):
+        """Delete all backups made by WP Complete Backup."""
+        self.cwd('/wp-content/plugins/wp-complete-backup/storage')
+        files = self.get_file_list()
+        for f in files:
+            if f['dict']:
+                self.delete_folder(f['name'])
+
+    def make_wp_complete_backup(self, url):
+        """Query the remote execution URL, which will make a database
+        backup."""
+        urllib.request.urlopen(url)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+if config['backup'].getboolean('wp_complete_backup'):
+    ftp = AryaStarkFtp(config['ftp']['host'], config['ftp']['username'],
+                       config['ftp']['password'])
+    if config['wp_complete_backup'].getboolean('delete_previous_backups'):
+        ftp.delete_wp_complete_backups()
+    ftp.make_wp_complete_backup(config['wp_complete_backup']['url'])
+    ftp.quit()
+
+if config['backup'].getboolean('ftp'):
+    ftp = AryaStarkFtp(config['ftp']['host'], config['ftp']['username'],
+                       config['ftp']['password'])
+    ftp.save_files_in_folder()
+    ftp.quit()
